80d08f8b16f1b86133936c25d6656ffae1b2027e
twfahey1 <twfahey@me.com>
2014-12-16 23:37:49 -0500

    Ship moves around w Actor buttons Will show move buttons on Android, on Desktop use arrows Alt+Enter for Full screen Ctrl shoots out weapon, [ ] buttons change weapons, dif. shapes, needs a LOT of work

--- desktop/src/com/fahey/galaxyexp/desktop/DesktopLauncher.java
+++ desktop/src/com/fahey/galaxyexp/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.fahey.galaxyexp.desktop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.fahey.galaxyexp.GalaxyExplorerMain;
@@ -7,6 +8,9 @@ import com.fahey.galaxyexp.GalaxyExplorerMain;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title="Galaxy Explorer BETA";
+		config.width = 1080;
+		config.height = 720;
 		new LwjglApplication(new GalaxyExplorerMain(), config);
 	}
 }
